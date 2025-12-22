661c8d05c986731f5d6618f21e49117dc2ff2d33
Ilya Borisenko <ilyaborisenko99@gmail.com>
2025-12-22 14:26:38 +0300

    hardcode HomeScreen

--- app/src/main/java/com/example/pexelsapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pexelsapp/ui/screen/HomeScreen.kt
@@ -12,6 +12,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pexelsapp.ui.components.Headers
+import com.example.pexelsapp.ui.components.ImagesGrid
+import com.example.pexelsapp.ui.components.PhotoUI
+import com.example.pexelsapp.ui.components.Sample
 import com.example.pexelsapp.ui.components.SearchBar
 
 @Preview
@@ -34,6 +38,47 @@ fun HomeScreen(){
                 query = newValue
             }
         )
+
+        val listOfTopics = listOf(
+            Sample ("Ice", true),
+            Sample ("Watches", false),
+            Sample ("Drawing", false),
+            Sample ("Brick", false),
+            Sample ("Architecture", false),
+        )
+
+        Headers(list = listOfTopics) {
+
+        }
+
+        val listOfPhotos = listOf(
+            PhotoUI(
+                url = "https://images.pexels.com/photos/716658/pexels-photo-716658.jpeg",
+                width = 6100,
+                height = 4067,
+                photographer = "Andrea Piacquadio"
+            ),
+            PhotoUI(
+                url = "https://images.pexels.com/photos/3444345/pexels-photo-3444345.png",
+                width = 4000,
+                height = 6000,
+                photographer = "Kristina Paukshtite"
+            ),
+            PhotoUI(
+                url = "https://images.pexels.com/photos/5728187/pexels-photo-5728187.jpeg",
+                width = 6000,
+                height = 3783,
+                photographer = "Any Lane"
+            ),
+            PhotoUI(
+                url = "https://images.pexels.com/photos/1303094/pexels-photo-1303094.jpeg",
+                width =  5616,
+                height = 3744,
+                photographer = "George Dolgikh"
+            )
+        )
+
+        ImagesGrid(photosList = listOfPhotos)
 
     }
 }
