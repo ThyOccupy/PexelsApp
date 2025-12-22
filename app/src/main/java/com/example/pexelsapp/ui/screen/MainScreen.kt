6142cd28730f2de33fa28ced3a4ff7cd48c35b99
Ilya Borisenko <ilyaborisenko99@gmail.com>
2025-12-22 13:46:37 +0300

    plane realization Jetpack Compose Navigation

--- app/src/main/java/com/example/pexelsapp/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/pexelsapp/ui/screen/MainScreen.kt
@@ -1,8 +1,28 @@
 package com.example.pexelsapp.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.pexelsapp.ui.components.BottomBar
+import com.example.pexelsapp.ui.navigation.NavGraph
+import com.example.pexelsapp.ui.navigation.rememberNavigationState
 
 @Composable
-fun MainScreen(){
-    HomeScreen()
+fun MainScreen() {
+    val navigationState = rememberNavigationState()
+
+    Scaffold(
+        bottomBar = { BottomBar(navigationState) }
+    ) { paddingValues ->
+
+        NavGraph(
+            navHostController = navigationState.navHostController,
+            homeScreenContent = { HomeScreen() },
+            bookmarkScreenContent = { Text(text = "BookmarkScreen", color = Color.Black) },
+            modifier = Modifier.padding(paddingValues)
+        )
+    }
 }
