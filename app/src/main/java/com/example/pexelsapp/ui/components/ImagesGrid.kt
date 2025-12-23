d57185af5940f177d04d001622358902b6d3ea03
Ilya Borisenko <ilyaborisenko99@gmail.com>
2025-12-23 18:39:33 +0300

    add detailed error handling

--- app/src/main/java/com/example/pexelsapp/ui/components/ImagesGrid.kt
+++ app/src/main/java/com/example/pexelsapp/ui/components/ImagesGrid.kt
@@ -1,5 +1,7 @@
 package com.example.pexelsapp.ui.components
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -18,10 +20,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -104,6 +108,34 @@ fun NoResultsStub(
 ) {
     when {
         photosList.loadState.refresh is LoadState.NotLoading && photosList.itemCount == 0 -> {
+
+            val context = LocalContext.current
+            LaunchedEffect(key1 = photosList.loadState){
+                if(photosList.loadState.refresh is LoadState.Error) {
+                    val error = (photosList.loadState.refresh as LoadState.Error).error.apply {
+                        when (this) {
+                            is retrofit2.HttpException -> when (this.code()) {
+                                400 -> R.string.error_400
+                                401 -> R.string.error_400
+                                403 -> R.string.error_400
+                                404 -> R.string.error_400
+                                429 -> R.string.error_400
+                                500 -> R.string.error_400
+                                502 -> R.string.error_400
+                                else -> "Failed to retrieve data. Error: ${this.message}"
+                            }
+                            is java.net.UnknownHostException -> R.string.error_unknown_host
+                            is java.net.ConnectException -> R.string.error_connect
+                            is java.net.SocketTimeoutException -> R.string.error_timeout
+                            else -> "Unexpected Error: ${this.message ?: "Unknown error"}"
+                        }
+                    }
+
+                    Toast.makeText(context, "Error: $error", Toast.LENGTH_LONG).show()
+                    Log.d("LoadError", "Error loading: $error")
+                }
+            }
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
