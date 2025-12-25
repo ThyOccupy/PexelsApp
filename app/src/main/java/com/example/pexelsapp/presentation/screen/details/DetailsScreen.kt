7b54f0a8f66f64412434abdb1d6ed1b36d9bb243
Ilya Borisenko <ilyaborisenko99@gmail.com>
2025-12-25 22:21:22 +0300

    update hilt dependency

--- app/src/main/java/com/example/pexelsapp/presentation/screen/details/DetailsScreen.kt
+++ app/src/main/java/com/example/pexelsapp/presentation/screen/details/DetailsScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.pexelsapp.R
 import com.example.pexelsapp.presentation.common.components.photo.PhotoCard
 import com.example.pexelsapp.presentation.common.components.button.BackButton
--- app/src/main/java/com/example/pexelsapp/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/pexelsapp/presentation/screen/home/HomeScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
